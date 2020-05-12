b2698b278dd90e24daf82f26bab9a29f0a5356eb
Jonatan Waldow <jonatan.waldow@moveresoftware.com>
2020-05-12 13:41:43 -0400

    Initial commit.

---
+++ src/CursoDesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CursoDesignPatterns
+{
+	public class CalculadorDeImpostos
+	{
+		public void RealizarCalculo(Orcamento orcamento, string imposto)
+		{
+			if (imposto.Equals("ICMS"))
+			{
+				double icms = orcamento.Valor * 0.1;
+				Console.WriteLine(icms);
+			} else if (imposto.Equals("ISS"))
+			{
+				double iss = orcamento.Valor * 0.06;
+				Console.WriteLine(iss);
+			}			
+		}
+	}
+}
