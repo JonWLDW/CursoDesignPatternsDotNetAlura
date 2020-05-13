1af21519cbd940cfb28347ceb84161d26456f8bf
JonWLDW <jonwldw@gmail.com>
2020-05-13 18:23:57 -0400

    Criada seleção de rotina para separar os exercícios.

--- src/CursoDesignPatterns/Program.cs
+++ src/CursoDesignPatterns/Program.cs
@@ -6,8 +6,28 @@ namespace CursoDesignPatterns
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
+			Console.WriteLine("Informe o número da rotina!");
+			var rotina = Console.ReadLine();
+
+			switch (rotina)
+			{
+				case "1":
+					RotinaDeCalculoDeImpostosNoPadraoStrategy();
+					break;
+					
+				default:
+					Console.WriteLine("Rotina inválida!");
+					Main();
+					break;
+			}
+		}
+
+		private static void RotinaDeCalculoDeImpostosNoPadraoStrategy()
+		{
+			Console.WriteLine("Iniciada rotina de cálculo de desconto no padrão Strategy.\n");
+
 			Imposto iss = new ISS();
 			Imposto icms = new ICMS();
 			Imposto iccc = new ICCC();
@@ -16,7 +36,9 @@ namespace CursoDesignPatterns
 			calculador.RealizarCalculo(orcamento, icms);
 			calculador.RealizarCalculo(orcamento, iss);
 			calculador.RealizarCalculo(orcamento, iccc);
-			Console.ReadLine();
+
+			Console.WriteLine("\nFim da rotina de cálculo de desconto no padrão Strategy.");
+			Main();
 		}
 	}
 }
